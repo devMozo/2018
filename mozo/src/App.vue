e2f22e74740cbc8553af05005b258136ffe6ae16
devMozo <devnicolasmozo@gmail.com>
2018-10-03 23:47:41 -0300

    TP5 - Nicolas Mozo
    Removed internal README
    
    Finished the first release
    
    Modularized Form's fields
    
    Finished

---
+++ mozo/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+    <main>
+        <FormUser v-on:created="newUser"></FormUser>
+
+        <section class="wrapper-user materia-shadow">
+
+            <FilterSexUser v-on:updateSex="updateActiveSex" :mode-searcher="true"></FilterSexUser>
+
+            <ListUser :hola="hola" :sex-filter="sexFilter" :users="users"></ListUser>
+
+        </section>
+    </main>
+</template>
+
+<script>
+
+import FilterSexUser from '@/components/Filters/FilterSexUser'
+import FormUser from '@/components/FormUser/FormUser'
+import ListUser from '@/components/ListUser/ListUser'
+
+export default {
+  name: 'App',
+  components: {
+    FilterSexUser,
+    FormUser,
+    ListUser
+  },
+  data: function () {
+    return {
+      users: [],
+      sexFilter: ''
+    }
+  },
+  methods: {
+    newUser (user) {
+      this.users.push(user)
+    },
+    updateActiveSex (result) {
+      this.sexFilter = result
+    },
+    hola (msg) {
+      alert(msg)
+    }
+  }
+}
+</script>
+
+<style>
+    *{ font-family: 'Arial'; color: #444 }
+    input, select, button{ width: 100%; padding: 15px; margin-top: 10px; box-sizing: border-box;  }
+
+    main > *{ display: inline-block; vertical-align: top; }
+
+    .materia-shadow{ box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23); display: inline-block; }
+
+    .wrapper-user{ width: calc(100% - 475px); margin-top: 20px; padding: 10px 30px 20px; box-sizing: border-box; display: inline-block; }
+    .wrapper-user > ul{ clear: both; }
+    .wrapper-user select{ width: 200px; float: right; }
+</style>
---
+++ mozo/src/components/ListUser/ListUser.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul>
+        <p v-if="users.filter(user => {
+            return sexFilter == 'all' ||  user.sex.toLowerCase() == sexFilter.toLowerCase()
+        }).length == 0"> No user added to this gender </p>
+        <ItemUser
+            v-for="user in users"
+            v-if="sexFilter == 'all' || user.sex.toLowerCase() == sexFilter.toLowerCase()"
+            :user="user"
+            :key="user.id"></ItemUser>
+    </ul>
+</template>
+
+<script>
+
+import ItemUser from '@/components/ListUser/ItemUser'
+
+export default {
+  name: 'ListUser',
+  components: {
+    ItemUser
+  },
+  props: ['users', 'sexFilter']
+}
+
+</script>
+
+<style>
+</style>
